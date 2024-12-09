e9bb31576ea95aeabc8488377e2da1e53a0aa758
Deepak Sriram <dsriram@griddynamics.com>
2024-12-09 19:35:50 +0530

    Modified Services with CrudService

--- pet-clinic-data/src/main/java/com/deepak/pet_clinic/service/PetService.java
+++ pet-clinic-data/src/main/java/com/deepak/pet_clinic/service/PetService.java
@@ -2,13 +2,6 @@ package com.deepak.pet_clinic.service;
 
 import com.deepak.pet_clinic.model.Pet;
 
-import java.util.List;
-
-public interface PetService {
-    Pet findById(long id);
-
-    List<Pet> findAll();
-
-    Pet save(Pet pet);
+public interface PetService extends CrudService<Pet, Long>{
 
 }
