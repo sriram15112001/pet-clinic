62c83b1bfc5e83960c585a5d1e99311079492352
Deepak Sriram <dsriram@griddynamics.com>
2024-12-10 13:58:00 +0530

    Added map services

---
+++ pet-clinic-data/src/main/java/com/deepak/pet_clinic/service/map/PetServiceMap.java
@@ -0,0 +1,23 @@
+package com.deepak.pet_clinic.service.map;
+
+import com.deepak.pet_clinic.model.Pet;
+import com.deepak.pet_clinic.service.PetService;
+
+import java.util.List;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet pet) {
+        return super.save(pet.getId(), pet);
+    }
+
+    @Override
+    public List<Pet> findAll() {
+        return super.findAll();
+    }
+}
